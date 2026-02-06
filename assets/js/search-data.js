ca128a47ada461f675ddba4978ee1ca67c37addd
hamishaider <76895811+hamishaider@users.noreply.github.com>
2026-02-06 07:25:01 +0000

    Deploying to gh-pages from @ hamishaider/hamishaider.github.io@51d77c3113465e8639db529ad867c9685befd6b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -390,9 +390,9 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-approximate-computation-units",
+          title: 'Approximate Computation Units',
+          description: "Novel Compute Units for enabling efficient AI at Edge",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
