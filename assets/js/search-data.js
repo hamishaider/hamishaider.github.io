7994000c15e1e37e24493c16db2802a82e7aac5c
hamishaider <76895811+hamishaider@users.noreply.github.com>
2026-02-07 04:13:59 +0000

    Deploying to gh-pages from @ hamishaider/hamishaider.github.io@2212cfac0bebf1311fb732858d25fb6ef0fa1731 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -433,7 +433,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv.pdf", "_blank");
+          window.open("/assets/rendercv/rendercv_output/CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
